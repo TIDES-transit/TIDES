a77d469614b11689c8b9cdd285329790bdf80b83
Reid <botanize@users.noreply.github.com>
2022-09-22 09:36:16 -0500

    stabilize sample data date

--- samples/50027-MetroTransitMN/data-raw/extract.R
+++ samples/50027-MetroTransitMN/data-raw/extract.R
@@ -5,14 +5,15 @@ library(odbc)
 
 ## Setup ####
 RAW_PATH = file.path('samples', '50027-MetroTransitMN', 'data-raw')
+DATE = as.Date('2022-09-20')
+CALENDAR_ID = as.integer(strftime(DATE, format = '1%Y%m%d'))
 
 ## Event data ####
 tmdl = dbConnect(odbc(), "TMDailyLog")
 odbcSetTransactionIsolationLevel(tmdl, 'read_uncommitted')
 
 ## vehicle locations
-# result = data.table(dbGetQuery(tmdl, "SELECT logged_message_id location_ping_id, calendar_id - 1e8 date, MESSAGE_TIMESTAMP timestamp, source_host vehicle_id, latitude * 1e-7 latitude, longitude * 1e-7 longitude, validity & 15 FOM, heading bearing, speed * 0.44704 speed, CAST(odometer * 16.09344 AS INTEGER) odometer, adherence schedule_deviation FROM logged_message_short WHERE message_type_id = 12 AND calendar_id BETWEEN ? AND ?", params = as.integer(strftime(c(start_date, end_date), '1%Y%m%d'))))
-vehicle_locations = data.table(dbGetQuery(tmdl, "SELECT TOP 1000 logged_message_short_id, calendar_id, message_timestamp, source_host, latitude, longitude, validity, heading, speed, odometer, adherence_seconds FROM logged_message_short WHERE message_type_id = 12 AND calendar_id = ?", params = as.integer(strftime(Sys.Date(), '1%Y%m%d'))), key = 'logged_message_short_id')
+vehicle_locations = data.table(dbGetQuery(tmdl, "SELECT TOP 1000 logged_message_short_id, calendar_id, message_timestamp, source_host, latitude, longitude, validity, heading, speed, odometer, adherence_seconds FROM logged_message_short WHERE message_type_id = 12 AND calendar_id = ?", params = CALENDAR_ID), key = 'logged_message_short_id')
 fwrite(vehicle_locations, file.path(RAW_PATH, 'vehicle_locations.csv'))
 
 ## passenger events
